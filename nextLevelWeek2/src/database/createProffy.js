f45162eb0b4f2b82c1c04e008197d3fa81f4f19b
fcdennis <fcdennis@gmail.com>
2020-09-28 14:29:17 -0300

    week2

--- nextLevelWeek2/src/database/createProffy.js
+++ nextLevelWeek2/src/database/createProffy.js
@@ -1,4 +1,4 @@
-module.exports = async function (db, { proffyValue, classValue, classScheduleValues }) {
+module.exports = async function (db, { proffyValue, classesValue, classScheduleValues }) {
     // inserir dados na table de proffys
     const insertedProffy = await db.run(`
         INSERT INTO proffys(
@@ -17,37 +17,37 @@ module.exports = async function (db, { proffyValue, classValue, classScheduleVal
     const proffy_id = insertedProffy.lastID
 
     //inserir dados na tabela classes
-    const insertedClass = await db.run(`
+    const insertedClasses = await db.run(`
             INSERT INTO classes(
                 subject,
                 cost,
                 proffy_id
             ) VALUES(
-                "${classValue.subject}",
-                "${classValue.cost}",
+                "${classesValue.subject}",
+                "${classesValue.cost}",
                 "${proffy_id}"
             );
     `)
 
-    const class_id = insertedClass.lastID
-
-    //Inserir dados na table class_schedule
-    const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+    const class_id = insertedClasses.lastID
+    
+    //Inserir dados na tabela class_schedule
+    const insertedAllclassScheduleValues = classScheduleValues.map((classScheduleValue) => {
         return db.run(`
-            INSERT INTO class_schedule (
-                class_id,
-                weekday,
-                time_from,
-                time_to
-            ) VALUES("
-                "${class_id}",
-                "${classScheduleValue.weekday}",
-                "${classScheduleValue.time_from}",
-                "${classScheduleValue.time_to}"
-            );
+        INSERT INTO class_schedule (
+            class_id,
+            weekday,
+            time_from,
+            time_to
+        ) VALUES (
+            "${class_id}",
+            "${classScheduleValue.weekday}",
+            "${classScheduleValue.time_from}",
+            "${classScheduleValue.time_to}"
+        )        
         `)
     })
 
-    //aqui executa todos os db.runs() das class_values
-    await Promise.all(insertedAllClassScheduleValues)
+
+    await Promise.all(insertedAllclassScheduleValues)
 }
--- nextLevelWeek2/src/database/test.js
+++ nextLevelWeek2/src/database/test.js
@@ -7,29 +7,59 @@ Database.then(async (db) => {
     proffyValue = {
         name: "Dennis Costa",
         avatar: "https://avatars2.githubusercontent.com/u/56238317?s=460&v=4",
-        whatsapp: "11881154410",
+        whatsapp: "89987654534",
         bio: "Melhor professor",
     }
 
-    classValue = {
+    classesValue = {
         subject: "História",
         cost: "500",
         //proffy id vira pelo db
     }
 
     classScheduleValues = [
-        //class_id vira pelo db
+        //class_id virá pelo banco de dados, após cadastrarmos a class
         {
             weekday: 1,
             time_from: 720,
-            time_to: 920
+            time_to: 1220
         },
         {
             weekday: 0,
             time_from: 520,
-            time_to: 920
+            time_to: 1220
         }
     ]
 
-    await createProffy(db,{proffyValue, classValue, classScheduleValues})
+    //await createProffy(db, {proffyValue, classesValue, classScheduleValues})
+
+    //Consultar os dados inseridos
+    
+    //todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    //console.log(selectedProffys)
+
+    //consultar as classes de um determinado professor
+    //e trazer junto os dados do professor
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM classes
+        JOIN proffys ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    //console.log(selectClassesAndProffys)
+
+    //
+    //
+    //
+    const selectClassesSchedules = await db.all(`
+    SELECT class_schedule.*
+    FROM class_schedule
+    WHERE class_schedule.class_id = 1    
+    `)
+
+    console.log(selectClassesSchedules)
+
+
+
 })
